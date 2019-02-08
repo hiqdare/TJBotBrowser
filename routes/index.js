1feb97c6db0acd648d339c19dcf81478ade2e1a8
adrianburki <burki.adrian@gmail.com>
2019-02-08 11:03:36 +0100

    added app version to the title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let PackageJSON = require("../package.json");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { appversion: PackageJSON.version });
 });
 
 module.exports = router;
